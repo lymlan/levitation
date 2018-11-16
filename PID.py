ba5a013ee6b5b55c30784f9cdc1a730db0b540d5
lymlan <j.emil.hansen@hotmail.com>
2018-11-16 15:44:27 +0000

    fix tabs and spaces

--- PID.py
+++ PID.py
@@ -5,88 +5,88 @@ import numpy as np
 
 class PID(object):
 
-	def __init__(self, pwm, i2c, v_max, v_min, KP=600, KI=0.001, KD=10):
-		self.pwm = pwm
-		self.i2c = i2c
-		self.v_max = v_max
-		self.v_min = v_min
-		self.KP = KP	# made these initialized variables so GA can work with PID
-		self.KI = KI
-		self.KD = KD
+    def __init__(self, pwm, i2c, v_max, v_min, KP=600, KI=0.001, KD=10):
+        self.pwm = pwm
+        self.i2c = i2c
+        self.v_max = v_max
+        self.v_min = v_min
+        self.KP = KP    # made these initialized variables so GA can work with PID
+        self.KI = KI
+        self.KD = KD
 
-	def force_normaliser(self, location, G):
-	    '''As the attractive force depends on the distance from the magnet, the force should be normalised to compensate. 
-	    The force of the electromagnet is dependent on the square of the distance the maximum current should be proportional to the max distance
-	    '''
+    def force_normaliser(self, location, G):
+        '''As the attractive force depends on the distance from the magnet, the force should be normalised to compensate. 
+        The force of the electromagnet is dependent on the square of the distance the maximum current should be proportional to the max distance
+        '''
 
-	#checking the reading for errors
-	    if location < self.v_min - 0.5:
-		raise ValueError(location, self.v_min, 'position read as above the maximum value, probably problem with light source.')
-	    operation_range = self.v_max - self.v_min
-	#top location is v_min, bottom location is v_max (because a higher position means a larger shadow on the PV cell.)
-	    normalised_distance_from_top = (location - self.v_min) / operation_range
-	    if normalised_distance_from_top > 1.5:
+    #checking the reading for errors
+        if location < self.v_min - 0.5:
+        raise ValueError(location, self.v_min, 'position read as above the maximum value, probably problem with light source.')
+        operation_range = self.v_max - self.v_min
+    #top location is v_min, bottom location is v_max (because a higher position means a larger shadow on the PV cell.)
+        normalised_distance_from_top = (location - self.v_min) / operation_range
+        if normalised_distance_from_top > 1.5:
                 raise ValueError(normalised_distance_from_top, 'NDFT is greater than 1')
             elif normalised_distance_from_top > 1:
                 normalised_distance_from_top = 1
-	    force = G * normalised_distance_from_top ** 2
-    	    self.pwm.DC(force)
+        force = G * normalised_distance_from_top ** 2
+            self.pwm.DC(force)
 
-	def position(self, position):
-		target_position = position
-		i = 0
-		error_past = 0
-		Integral = 0
-		location = np.zeros(1000) # for csv data -> will have 1000 datapoints per chromosome
-		G = 0
-		peak_limit = 100
-		
-		current_position = self.i2c.getVoltage()
-		location[i] = current_position
-		
-		while ((current_position > self.v_min) and (i > 3)) and (current_position < self.v_max): # i > 3 ensures code doesn't stop at the start
-			try:
-				current_position = self.i2c.getVoltage()
-				location[i] = current_position
-				error = (current_position - target_position)/ target_position
+    def position(self, position):
+        target_position = position
+        i = 0
+        error_past = 0
+        Integral = 0
+        location = np.zeros(1000) # for csv data -> will have 1000 datapoints per chromosome
+        G = 0
+        peak_limit = 100
+        
+        current_position = self.i2c.getVoltage()
+        location[i] = current_position
+        
+        while ((current_position > self.v_min) and (i > 3)) and (current_position < self.v_max): # i > 3 ensures code doesn't stop at the start
+            try:
+                current_position = self.i2c.getVoltage()
+                location[i] = current_position
+                error = (current_position - target_position)/ target_position
 
-				V = error - error_past
-				D = self.KD * V
+                V = error - error_past
+                D = self.KD * V
 
-				P = error * self.KP
-	            if G != peak_limit:
-		    		Integral += error
-		    	I = Integral * self.KI
+                P = error * self.KP
+                if G != peak_limit:
+                    Integral += error
+                I = Integral * self.KI
 
-				G = P + I + D
+                G = P + I + D
 
-				if G > peak_limit:
-					G = peak_limit
-				elif G < 0:
-					G = 0
+                if G > peak_limit:
+                    G = peak_limit
+                elif G < 0:
+                    G = 0
 
-				self.force_normaliser(location[i], G)
+                self.force_normaliser(location[i], G)
 
-				i +=1
-				error = error_past
-			except Exception as e: # predicting an error if it lasts more than 1000 cycles
-				print(e)
+                i +=1
+                error = error_past
+            except Exception as e: # predicting an error if it lasts more than 1000 cycles
+                print(e)
 
-		return location
+        return location
 
-#		KP = 600	# Emil's PID parameter values... should probably keep these just in case!
-#		KI = 0.001
-#		KD = 10
+#       KP = 600    # Emil's PID parameter values... should probably keep these just in case!
+#       KI = 0.001
+#       KD = 10
 
-		
+        
 
 
-		# length = np.transpose(np.linspace(0,999,1000))
-		# location = np.transpose(location)
-		# myData = [length, location]  
-		# date = datetime.datetime.now().strftime("%H-%M-%S-%B-%d-%Y")	
-	 #        filename = './logs/PID-Response-' + date +'.csv'
+        # length = np.transpose(np.linspace(0,999,1000))
+        # location = np.transpose(location)
+        # myData = [length, location]  
+        # date = datetime.datetime.now().strftime("%H-%M-%S-%B-%d-%Y")  
+     #        filename = './logs/PID-Response-' + date +'.csv'
   #               myFile = open(filename, 'w')  
-		# with myFile:  
-  #  			writer = csv.writer(myFile)
-  #  			writer.writerows(myData)
+        # with myFile:  
+  #             writer = csv.writer(myFile)
+  #             writer.writerows(myData)
--- ga.py
+++ ga.py
@@ -5,19 +5,18 @@ Modified GA code from SOURCE
 
 import random
 import math
-import listtools # is this a real module?
+import numpy as np
 import csv
 import time
 import RPi.GPIO as GPIO
 import curses
 import i2c
-import controller
 import calibrate
 import PID
 
 MAX_TIMESTEPS = 150
 POPULATION_SIZE = 50 # 100 (my values are from a paper on GAs for magnetic levitation PID tuning...)
-					 # http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.421.36&rep=rep1&type=pdf
+                     # http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.421.36&rep=rep1&type=pdf
 MUTATION_PROBABILITY = 0.33 # 0.1
 CROSSOVER_RATE = 0.9 # 0.7
 MAX_RUNS = 100 # generations
@@ -26,11 +25,11 @@ MAX_GAIN_VALUE = 1
 LINE_SMOOTHNESS = .1
 
 class Chromosome:
-	def __init__(self, kp, kd, ki):
-		self.kp = kp
-		self.kd = kd
-		self.ki = ki
-	 
+    def __init__(self, kp, kd, ki):
+        self.kp = kp
+        self.kd = kd
+        self.ki = ki
+     
 
 """
 1 [Start] Generate random population of n chromosomes (suitable solutions for the problem)
@@ -38,14 +37,14 @@ Creates a random genome
 
 """
 def generate_initial_population():
-	print("Generating initial population...\n")
-	random.seed()
-	population = np.zeros(POPULATION_SIZE)
-	for i in range(POPULATION_SIZE):
-		# create a random chromosome with a random gain value
-		population[i] = Chromosome(random.random() * MAX_GAIN_VALUE, random.random() * MAX_GAIN_VALUE, random.random() * MAX_GAIN_VALUE)
-	return population
-		
+    print("Generating initial population...\n")
+    random.seed()
+    population = np.zeros(POPULATION_SIZE)
+    for i in range(POPULATION_SIZE):
+        # create a random chromosome with a random gain value
+        population[i] = Chromosome(random.random() * MAX_GAIN_VALUE, random.random() * MAX_GAIN_VALUE, random.random() * MAX_GAIN_VALUE)
+    return population
+        
 """ 
 2 [Fitness] Evaluate the fitness f(x) of each chromosome x in the population
 returns the fitness value according to the fitness function
@@ -54,89 +53,89 @@ Fitness is how long the ball remains inbetween the top and bottom without touchi
 
 """
 def fitness(samples): # **MAKE THIS WORK...**
-	# 1. get top and bottom voltage values (as we already do)
-	# 2. measure voltage at every time interval (as we already do)
-	# 3. while measured voltage != (top or bottom voltages): keep going, don't cap at 1000 samples (iter ++)
-	# 4. when measured voltage == (top of bottom voltages): fitness = number of samples before this happened
-	#														this might take a while to get going though...
-	sum = 0
-	for i in range(len(distance_list)):
-		sum = sum + math.fabs(distance_list[i])
-	return 1 /  sum
-		
+    # 1. get top and bottom voltage values (as we already do)
+    # 2. measure voltage at every time interval (as we already do)
+    # 3. while measured voltage != (top or bottom voltages): keep going, don't cap at 1000 samples (iter ++)
+    # 4. when measured voltage == (top of bottom voltages): fitness = number of samples before this happened
+    #                                                       this might take a while to get going though...
+    sum = 0
+    for i in range(len(distance_list)):
+        sum = sum + math.fabs(distance_list[i])
+    return 1 /  sum
+        
 """
 Run simulation for a specific chromosome c.
 
 Returns the fitness function value of the simulation
 """
 def run_simulation_for_chromosome(population, chromosome): # **MAKE THIS WORK AND DO OTHER IMPORTANT STUFF LIKE LOGGING**
-	
-	fitness = 0
-	pid = PID.PID(pwm, i2c, v_max, v_min, chomosome.kp, chomosome.ki, chromosome.kd, position) # custom PID for chromosome k values
-	print("\nkp = {}\nki = {}\nkd = {}\n".format(chromosome.kp, chromosome.ki, chromosome.kd))
-	location_data = pid.position(location) # performs PID control and returns location data for ball
-	
-	# logging
-	# length = np.transpose(np.linspace(0,999,1000))
-	# location = np.transpose(location)
-	# myData = [length, location]  
-	# date = datetime.datetime.now().strftime("%H-%M-%S-%B-%d-%Y")	
-	# filename = './logs/PID-Response-' + chromosome + date +'.csv' # this won't work for multiple generations (it's temporary)
-	# myFile = open(filename, 'w')
-	# with myFile:  
-	# 	writer = csv.writer(myFile)
-	# 	writer.writerows(myData)
+    
+    fitness = 0
+    pid = PID.PID(pwm, i2c, v_max, v_min, chomosome.kp, chomosome.ki, chromosome.kd, position) # custom PID for chromosome k values
+    print("\nkp = {}\nki = {}\nkd = {}\n".format(chromosome.kp, chromosome.ki, chromosome.kd))
+    location_data = pid.position(location) # performs PID control and returns location data for ball
+    
+    # logging
+    # length = np.transpose(np.linspace(0,999,1000))
+    # location = np.transpose(location)
+    # myData = [length, location]  
+    # date = datetime.datetime.now().strftime("%H-%M-%S-%B-%d-%Y")  
+    # filename = './logs/PID-Response-' + chromosome + date +'.csv' # this won't work for multiple generations (it's temporary)
+    # myFile = open(filename, 'w')
+    # with myFile:  
+    #   writer = csv.writer(myFile)
+    #   writer.writerows(myData)
 
-	value = range_check(value)
-	pwm.DC(value)
+    value = range_check(value)
+    pwm.DC(value)
 
-	fitness += 1
+    fitness += 1
 
-	return fitness
-	
+    return fitness
+    
 """
 From Emil's code
-"""	
+""" 
 def range_check(value):
-	if value > 100:
-		value = 100
-	elif value < 0:
-		value = 0
-	return value
-	
+    if value > 100:
+        value = 100
+    elif value < 0:
+        value = 0
+    return value
+    
 """
 Run the simulation for the set of all chromosomes
 """
 def run_simulation(population):
-	fitness_values = np.zeros(POPULATION_SIZE)
-	for chromosome in range(POPULATION_SIZE):
-		fitness_values[chromosome] = run_simulation_for_chromosome(population, chromosome)
-		
-	return fitness_values
+    fitness_values = np.zeros(POPULATION_SIZE)
+    for chromosome in range(POPULATION_SIZE):
+        fitness_values[chromosome] = run_simulation_for_chromosome(population, chromosome)
+        
+    return fitness_values
 
 """
 Pick two parents according to probability represented by normalized fitness values
 3a[Selection] Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)
 """
 def selection(fitness_values):
-	# normalize the list so we have probabilities to pick parents
-	fitness_values = normListSumTo(fitness_values, 1)
-	parents = []
-	random.seed()
-	parent1_probability = random.random()
-	parent2_probability = random.random()
-	
-	sum = 0
-	for i in range(POPULATION_SIZE):
-		if len(parents) == 2:
-			break
-		next_sum = sum + fitness_values[i]
-		if parent1_probability <= next_sum and parent1_probability >= sum:
-			parents.append(i)
-		if parent2_probability <= next_sum and parent2_probability >= sum:
-			parents.append(i)
-		sum = next_sum
-	return parents
+    # normalize the list so we have probabilities to pick parents
+    fitness_values = normListSumTo(fitness_values, 1)
+    parents = []
+    random.seed()
+    parent1_probability = random.random()
+    parent2_probability = random.random()
+    
+    sum = 0
+    for i in range(POPULATION_SIZE):
+        if len(parents) == 2:
+            break
+        next_sum = sum + fitness_values[i]
+        if parent1_probability <= next_sum and parent1_probability >= sum:
+            parents.append(i)
+        if parent2_probability <= next_sum and parent2_probability >= sum:
+            parents.append(i)
+        sum = next_sum
+    return parents
 
 def normListSumTo(L, sumTo=1):
     '''normalize values of a list to make it sum = sumTo'''
@@ -149,107 +148,107 @@ def normListSumTo(L, sumTo=1):
 If no crossover was performed, offspring is an exact copy of parents.
 """
 def crossover(population, parents):
-	random.seed()
+    random.seed()
 
-	# if we dont crossover, offspring is a copy of parents
-	if random.random() > CROSSOVER_RATE:
-		return population[parents[0]]
-	else:
-		# random combination crossover
-		number = random.random()
-		if number < .25:
-			return Chromosome(population[parents[1]].kp,population[parents[1]].kd, population[parents[1]].ki)
-		elif number < .5:
-			return Chromosome(population[parents[0]].kp,population[parents[1]].kd, population[parents[1]].ki)
-		elif number < .75:
-			return Chromosome(population[parents[0]].kp,population[parents[0]].kd, population[parents[1]].ki)
-		else:
-			return Chromosome(population[parents[0]].kp,population[parents[0]].kd, population[parents[0]].ki)
+    # if we dont crossover, offspring is a copy of parents
+    if random.random() > CROSSOVER_RATE:
+        return population[parents[0]]
+    else:
+        # random combination crossover
+        number = random.random()
+        if number < .25:
+            return Chromosome(population[parents[1]].kp,population[parents[1]].kd, population[parents[1]].ki)
+        elif number < .5:
+            return Chromosome(population[parents[0]].kp,population[parents[1]].kd, population[parents[1]].ki)
+        elif number < .75:
+            return Chromosome(population[parents[0]].kp,population[parents[0]].kd, population[parents[1]].ki)
+        else:
+            return Chromosome(population[parents[0]].kp,population[parents[0]].kd, population[parents[0]].ki)
 
 """
 3c[Mutation] With a mutation probability mutate new offspring at each locus (position in chromosome).
 """
 def mutation(chromosome):
-	random.seed()
-	
-	if random.random() < MUTATION_PROBABILITY / 3:
-		#very small real valued mutation
-		chromosome.kp = chromosome.kp + random.random()/MAX_GAIN_VALUE 
-		if chromosome.kp < 0:
-			chromosome.kp = random.random() * MAX_GAIN_VALUE
-			
-	elif random.random() < MUTATION_PROBABILITY * 2/3:
-		chromosome.ki = chromosome.ki + random.random()/MAX_GAIN_VALUE
-		if chromosome.ki < 0:
-			chromosome.ki = random.random() * MAX_GAIN_VALUE
-			
-	elif random.random() < MUTATION_PROBABILITY:
-		chromosome.kd = chromosome.kd + random.random()/MAX_GAIN_VALUE
-		if chromosome.kd < 0:
-			chromosome.kd = random.random() * MAX_GAIN_VALUE		
-			
-	return chromosome
-	
+    random.seed()
+    
+    if random.random() < MUTATION_PROBABILITY / 3:
+        #very small real valued mutation
+        chromosome.kp = chromosome.kp + random.random()/MAX_GAIN_VALUE 
+        if chromosome.kp < 0:
+            chromosome.kp = random.random() * MAX_GAIN_VALUE
+            
+    elif random.random() < MUTATION_PROBABILITY * 2/3:
+        chromosome.ki = chromosome.ki + random.random()/MAX_GAIN_VALUE
+        if chromosome.ki < 0:
+            chromosome.ki = random.random() * MAX_GAIN_VALUE
+            
+    elif random.random() < MUTATION_PROBABILITY:
+        chromosome.kd = chromosome.kd + random.random()/MAX_GAIN_VALUE
+        if chromosome.kd < 0:
+            chromosome.kd = random.random() * MAX_GAIN_VALUE        
+            
+    return chromosome
+    
 """
 3 [New population] Create a new population by repeating following steps until the new population is complete
 """
 def generate_new_population(fitness_values, previous_population):
-	new_population = []
-	print("\nGenerating new population...\n")
-	for i in range(POPULATION_SIZE-1): # saves one space for elitist selection
-		new_population.append(i)
-		# selection
-		parents = selection(fitness_values)
-				
-		# crossover
-		chromosome = crossover(population, parents)
+    new_population = []
+    print("\nGenerating new population...\n")
+    for i in range(POPULATION_SIZE-1): # saves one space for elitist selection
+        new_population.append(i)
+        # selection
+        parents = selection(fitness_values)
+                
+        # crossover
+        chromosome = crossover(population, parents)
 
-		# mutation
-		chromosome = mutation(chromosome)
-		new_population[i] = chromosome
-	
-	
-	"""
-	Perform hybrid elitist selection. Carry the best chromosome over to the new population, unmutated.
-	"""
-	chromosome = population[max_index_in_list(fitness_values)]
-	new_population.append(POPULATION_SIZE-1)
-	new_population[POPULATION_SIZE-1] = chromosome
-	
-	return new_population
-	
+        # mutation
+        chromosome = mutation(chromosome)
+        new_population[i] = chromosome
+    
+    
+    """
+    Perform hybrid elitist selection. Carry the best chromosome over to the new population, unmutated.
+    """
+    chromosome = population[max_index_in_list(fitness_values)]
+    new_population.append(POPULATION_SIZE-1)
+    new_population[POPULATION_SIZE-1] = chromosome
+    
+    return new_population
+    
 def max_index_in_list(my_list):
-	max_value = max(my_list)
-	max_index = my_list.index(max_value)
-	return max_index
+    max_value = max(my_list)
+    max_index = my_list.index(max_value)
+    return max_index
 
 """
-	Main
+    Main
 
-	1 [Start] Generate random population of n chromosomes (suitable solutions for the problem)
-	2 [Fitness] Evaluate the fitness f(x) of each chromosome x in the population
-	3 [New population] Create a new population by repeating following steps until the new population is complete
-		3a[Selection] Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)
-		3b[Crossover] With a crossover probability cross over the parents to form a new offspring (children). If no crossover was performed, offspring is an exact copy of parents.
-		3c[Mutation] With a mutation probability mutate new offspring at each locus (position in chromosome).
-		3d[Accepting] Place new offspring in a new population
-	4 [Replace] Use new generated population for a further run of algorithm
-	5 [Test] If the end condition is satisfied, stop, and return the best solution in current population
-	6 [Loop] Go to step 2
+    1 [Start] Generate random population of n chromosomes (suitable solutions for the problem)
+    2 [Fitness] Evaluate the fitness f(x) of each chromosome x in the population
+    3 [New population] Create a new population by repeating following steps until the new population is complete
+        3a[Selection] Select two parent chromosomes from a population according to their fitness (the better fitness, the bigger chance to be selected)
+        3b[Crossover] With a crossover probability cross over the parents to form a new offspring (children). If no crossover was performed, offspring is an exact copy of parents.
+        3c[Mutation] With a mutation probability mutate new offspring at each locus (position in chromosome).
+        3d[Accepting] Place new offspring in a new population
+    4 [Replace] Use new generated population for a further run of algorithm
+    5 [Test] If the end condition is satisfied, stop, and return the best solution in current population
+    6 [Loop] Go to step 2
 """
-	
+    
 #################################################################################################################################################
-#  	
-#  	NOTE: Fitness_values is indexed by chromosome number, so if we attempt to sort it, 
+#   
+#   NOTE: Fitness_values is indexed by chromosome number, so if we attempt to sort it, 
 #   we will pick the wrong index when generating a new population. Which would be bad.
 #
 #################################################################################################################################################
 
-screen = curses.initscr()
-screen.refresh()
-curses.cbreak()
-screen.keypad(True)
-value = 1
+# screen = curses.initscr()
+# screen.refresh()
+# curses.cbreak()
+# screen.keypad(True)
+# value = 1
 
 generation = 1
 
@@ -261,32 +260,32 @@ population = generate_initial_population()
 fitness_values = run_simulation(population)
 
 try:
-	screen.addstr('press any key to start')
-	key = 0
+    # screen.addstr('press any key to start')
+    # key = 0
 
-	cal = calibrate.Calibrate(i2c, pwm)
-	v_min, v_max = cal.setup()
-	position = (v_min + v_max)/2
-	screen.addstr(str(v_max))
-	time.sleep(1)
+    cal = calibrate.Calibrate(i2c, pwm)
+    v_min, v_max = cal.setup()
+    position = (v_min + v_max)/2
+    screen.addstr(str(v_max))
+    time.sleep(1)
 except Exception as e:
-	print(e)
+    print(e)
 
 
 for i in range(MAX_RUNS):
-	print("Generation {}".format(generation))
-	population = generate_new_population(fitness_values, population)
-	fitness_values = run_simulation(population)
-	
-	max_value = max(fitness_values)
-	
-	print("Maximum fitness of generation {} = {}".format(generation, max_value))
+    print("Generation {}".format(generation))
+    population = generate_new_population(fitness_values, population)
+    fitness_values = run_simulation(population)
+    
+    max_value = max(fitness_values)
+    
+    print("Maximum fitness of generation {} = {}".format(generation, max_value))
 
-	# if the population sucks, DESTROY THE EARTH
-	if max_value < FITNESS_THRESHOLD:
-		print("Population sucked so we're starting with a fresh batch lol")
-		population = generate_initial_population()
-		generation = 1
-		continue
+    # if the population sucks, DESTROY THE EARTH
+    if max_value < FITNESS_THRESHOLD:
+        print("Population sucked so we're starting with a fresh batch lol")
+        population = generate_initial_population()
+        generation = 1
+        continue
 
-	generation += 1
+    generation += 1
